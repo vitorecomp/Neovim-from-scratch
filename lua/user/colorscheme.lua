656fa7c569858476458ff057957517f92b4b14f4
vitorecomp <vitor.ecomp@gmail.com>
2022-08-09 23:48:07 -0300

    feat: initial custumization

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
-local colorscheme = "tokyonight"
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
+--colorscheme dracula
+vim.cmd [[
+try
+  colorscheme darkplus
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
